2259713bb0f732c9264dbc4344e18b5f8ff62af8
Stefano <stefano.dipaola@mindedsecurity.com>
2018-04-28 11:34:14 +0200

    added update check and download as filname_new.js. This is a first attempt

--- src/standalone/patch.js
+++ src/standalone/patch.js
@@ -3,10 +3,15 @@ REMOVE ME TO BOTTOM TO RECREATE ORIGINAL
 FILE /usr/lib/slack/resources/app.asar.unpacked/src/static/ssb-interop.js
 */
 (function() {
+  try {
 
+  const branch = "feature/standaloneupdater";
+  const versionURL = `https://raw.githubusercontent.com/mindedsecurity/shhlack/${branch}/release/standalone/package.json`;
+  const updateURL = `https://raw.githubusercontent.com/mindedsecurity/shhlack/${branch}/release/standalone/shhlack.js`;
   const os = require('os');
   const path = require('path');
   const fs = require('fs');
+  const https = require('https');
 
   const shhlackHomePath = path.join(os.homedir(), '.shhlack');
   //Read Shhlack.js
@@ -33,10 +38,8 @@ FILE /usr/lib/slack/resources/app.asar.unpacked/src/static/ssb-interop.js
         }
     }
     return a.length - b.length;
-}
+   }
 
-  try {
-    const https = require('https');
     function httpget(url,onend,onerror,ondata,onresponse){
       https.get(url, (resp) => {
       let data = '';
@@ -46,7 +49,7 @@ FILE /usr/lib/slack/resources/app.asar.unpacked/src/static/ssb-interop.js
       // A chunk of data has been recieved.
       resp.on('data', (chunk) => {
         if(ondata)
-          ondata(chunk);
+         return ondata(chunk);
         data += chunk;
       });
 
@@ -62,12 +65,58 @@ FILE /usr/lib/slack/resources/app.asar.unpacked/src/static/ssb-interop.js
       //console.log("Error: " + err.message);
     });
     }
-    
-    httpget('https://raw.githubusercontent.com/mindedsecurity/shhlack/master/package.json?',function onend(data){
-        var version = JSON.parse(data).version;
-        if (cmpVersions(version, local_shhlackPackage.version) > 0) {
-          console.log("new Version!");
+    function setError(errorObject){
+        localStorage.shhlack_new_version = errorObject;
+    }
+    httpget(versionURL,function onend(data){
+        try{
+        var remote_version = JSON.parse(data).version;
+        var downloadfile;
+        if (cmpVersions(remote_version, local_shhlackPackage.version) > 0) {
+          console.log("Shhlack: New Version Available! Do you want to download and install the new version?");
+          localStorage.shhlack_new_version =  {version:local_shhlackPackage.version, new_version: remote_version};
+      httpget(updateURL,
+      function onend(){
+         try{
+          downloadfile.end();
+      fs.writeFileSync(path.join(shhlackHomePath, "package_new.json"),data);
+     }catch(exc){
+           console.error(exc);
+       setError({version:local_shhlackPackage.version, new_version: remote_version,error: exc.message, type: "update"});
+     }
+     },
+     function onerror(err){
+        console.error(err);       
+        setError({version:local_shhlackPackage.version, new_version: remote_version,error: err.message, type: "update"});
+     }, 
+     function ondata(chunk){
+       try{
+       downloadfile.write(chunk);
+       }catch(exc){
+        console.error(exc);
+        setError({version:local_shhlackPackage.version, new_version: remote_version,error: exc.message, type: "update"});
+       }
+     },
+     function onresponse(res){
+     try{
+      downloadfile = fs.createWriteStream( path.join(shhlackHomePath, "shhlack_new.js"), {
+            flags: 'w',
+            encoding: 'binary'
+          });
+     }catch(exc){
+        console.error(exc);
+       setError({version:local_shhlackPackage.version, new_version: remote_version,error: exc.message, type: "update"});
+     }
+     });
         }
+    }catch(exc){
+        console.error(exc);
+      setError({error: exc.message, type: "check_version" });
+    }
+
+    },function onerror(err){
+       console.error(err);
+       setError({error: err.message, type: "check_version" });
     });
 
   } catch (exc) {
