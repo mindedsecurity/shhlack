b0d33a57fdd9a2f9e36098bbb2658f63dab1a994
Stefano <stefano.dipaola@mindedsecurity.com>
2018-04-30 14:35:55 +0200

    latest release

--- release/standalone/patch.js
+++ release/standalone/patch.js
@@ -1,103 +1,183 @@
-
 /*PATCHED BY Minded Security to add encryption features. See shhlack
 REMOVE ME TO BOTTOM TO RECREATE ORIGINAL 
 FILE /usr/lib/slack/resources/app.asar.unpacked/src/static/ssb-interop.js
 */
 (function() {
-  const versionURL = "https://raw.githubusercontent.com/mindedsecurity/shhlack/master/package.json";
-  const updateURL = "https://github.com/mindedsecurity/shhlack/releases/download/|VERSION|/standalone-|VERSION|.zip"
-  const os = require('os');
-  const path = require('path');
-  const fs = require('fs');
+  try {
 
-  const shhlackHomePath = path.join(os.homedir(), '.shhlack');
-  //Read Shhlack.js
-  const shhlackPath = path.join(shhlackHomePath, 'shhlack.js');
-  const data = fs.readFileSync(shhlackPath) + '';
-  eval(data);
+    const branch = "master";
+    const versionURL = `https://raw.githubusercontent.com/mindedsecurity/shhlack/${branch}/release/standalone/package.json`;
+    const updateURL = `https://raw.githubusercontent.com/mindedsecurity/shhlack/${branch}/release/standalone/shhlack.js`;
+    const os = require('os');
+    const path = require('path');
+    const fs = require('fs');
+    const https = require('https');
 
-  //Read package.json to get version
-  const shhlackPackagePath = path.join(shhlackHomePath, 'package.json');
-  const local_shhlackPackage = require(shhlackPackagePath);
-  function cmpVersions(a, b, sep) {
-    var i,
+    const shhlackHomePath = path.join(os.homedir(), '.shhlack');
+    //Read Shhlack.js
+    const shhlackPath = path.join(shhlackHomePath, 'shhlack.js');
+    const shhlackJS_content = fs.readFileSync(shhlackPath) + '';
+    eval(shhlackJS_content);
+
+    //Read package.json to get version
+    const shhlackPackagePath = path.join(shhlackHomePath, 'package.json');
+    const local_shhlackPackage = require(shhlackPackagePath);
+    function cmpVersions(a, b, sep) {
+      var i,
         l,
         d;
-    sep = sep || ".";
-    a = a.split(sep);
-    b = b.split(sep);
-    l = Math.min(a.length, b.length);
+      sep = sep || ".";
+      a = a.split(sep);
+      b = b.split(sep);
+      l = Math.min(a.length, b.length);
 
-    for (i = 0; i < l; i++) {
+      for (i = 0; i < l; i++) {
         d = parseInt(a[i], 10) - parseInt(b[i], 10);
         if (d !== 0) {
-            return d;
+          return d;
         }
+      }
+      return a.length - b.length;
     }
-    return a.length - b.length;
-}
 
-  try {
-    const https = require('https');
-    function httpget(url,onend,onerror,ondata,onresponse){
+    function httpget(url, onend, onerror, ondata, onresponse) {
       https.get(url, (resp) => {
-      let data = '';
-      if(onresponse)
-        onresponse(resp);
+        var data = '';
+        if (onresponse)
+          onresponse(resp);
 
-      // A chunk of data has been recieved.
-      resp.on('data', (chunk) => {
-        if(ondata)
-         return ondata(chunk);
-        data += chunk;
-      });
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+          if (ondata)
+            return ondata(chunk);
+          data += chunk;
+        });
 
-      // The whole response has been received. Print out the result.
-      resp.on('end', () => {
-        if(onend)
-          onend(data);
-      });
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+          if (onend)
+            onend(data);
+        });
 
-    }).on("error", (err) => {
-      if(onerror)
-        onerror(err);
+      }).on("error", (err) => {
+        if (onerror)
+          onerror(err);
       //console.log("Error: " + err.message);
-    });
+      });
     }
-    
-    httpget(versionURL,function onend(data){
+    function setError(errorObject) {
+      localStorage.shhlack_new_version = errorObject;
+    }
+    httpget(versionURL, function onend(data) {
+      try {
         var remote_version = JSON.parse(data).version;
         var downloadfile;
-        if (cmpVersions(remote_version, local_shhlackPackage.version) > 0) {
-          console.log("Shhlack: New Version Available! Do you want to download and install the new version?");
-          localStorage.shhlack_new_version = {version:local_shhlackPackage.version, new_version: remote_version};
-      httpget(updateURL.replace("|VERSION|",remote_version),
-      function onend(){
-          downloadfile.end();
-     },
-     function onerror(err){
-        console.error(err);       
-        localStorage.shhlack_new_version = {version:local_shhlackPackage.version, new_version: remote_version,error: err.message, type: "update"};
-     }, 
-     function ondata(chunk){
-    downloadfile.write(chunk);
-     },
-     function onresponse(res){
-      downloadfile = fs.createWriteStream( path.join(shhlackHomePath, "shhlack.zip"), {
-            flags: 'w',
-            encoding: 'binary'
-          });
-       
-     });
-        }
+        var remote_version_array = remote_version.split('.');
+        var local_version_array = local_shhlackPackage.version.split('.');
 
-    },function onerror(err){
-       console.error(err);
-       localStorage.shhlack_new_version = {error: err.message, type: "check_version"};
+        var major_version = remote_version_array[0] !== local_version_array[0];
+        var minor_version = remote_version_array[1] !== local_version_array[1];
+        var patch_version = remote_version_array[2] !== local_version_array[2];
+        
+        // Expecting that if minor or major version change the patcher needs update.
+        // so user will have to download and install the whole package again
+        if (minor_version || major_version) {
+          alert(`Shhlack: New standalone version available ${remote_version} go to 
+            https://github.com/mindedsecurity/shhlack/releases/download/${remote_version}/standalone-${remote_version}.zip`);
+          return;
+        }
+        /*else if difference is on patch version only we'll 
+         just download download the new version on 
+         ~/.shhlack/package_new.json 
+        and 
+          ~/.shhlack/shhlack_new.json 
+        */
+        if (patch_version) {
+          console.log("Shhlack: New Version Available! Do you want to download and install the new version?");
+          localStorage.shhlack_new_version = {
+            version: local_shhlackPackage.version,
+            new_version: remote_version
+          };
+
+          httpget(updateURL,
+            function onend() {
+              try {
+                downloadfile.end(function() {
+                  fs.writeFileSync(path.join(shhlackHomePath, "package_new.json"), data);
+                  var response = confirm("Shhlack: New version downloaded, do you want to install it?");
+                  if (response) {
+                    fs.writeFileSync(shhlackPackagePath, fs.readFileSync(path.join(shhlackHomePath, "package_new.json")));
+                    console.log(shhlackPath, path.join(shhlackHomePath, "shhlack_new.js"), fs.readFileSync(path.join(shhlackHomePath, "shhlack_new.js")) + '')
+                    fs.writeFileSync(shhlackPath, fs.readFileSync(path.join(shhlackHomePath, "shhlack_new.js")) + '');
+                  }
+                });
+
+              } catch (exc) {
+                console.error(exc);
+                setError({
+                  version: local_shhlackPackage.version,
+                  new_version: remote_version,
+                  error: exc.message,
+                  type: "update"
+                });
+              }
+            },
+            function onerror(err) {
+              console.error(err);
+              setError({
+                version: local_shhlackPackage.version,
+                new_version: remote_version,
+                error: err.message,
+                type: "update"
+              });
+            },
+            function ondata(chunk) {
+              try {
+                downloadfile.write(chunk);
+              } catch (exc) {
+                console.error(exc);
+                setError({
+                  version: local_shhlackPackage.version,
+                  new_version: remote_version,
+                  error: exc.message,
+                  type: "update"
+                });
+              }
+            },
+            function onresponse(res) {
+              try {
+                downloadfile = fs.createWriteStream(path.join(shhlackHomePath, "shhlack_new.js"), {
+                  flags: 'w',
+                  encoding: 'binary'
+                });
+              } catch (exc) {
+                console.error(exc);
+                setError({
+                  version: local_shhlackPackage.version,
+                  new_version: remote_version,
+                  error: exc.message,
+                  type: "update"
+                });
+              }
+            });
+        }
+      } catch (exc) {
+        console.error(exc);
+        setError({
+          error: exc.message,
+          type: "check_version"
+        });
+      }
+
+    }, function onerror(err) {
+      console.error(err);
+      setError({
+        error: err.message,
+        type: "check_version"
+      });
     });
 
   } catch (exc) {
     console.error(exc)
   }
 })();
-
